5baf4e47c5e9840b08f15e4543ce90543811a80d
Tom Kirkpatrick <tom@systemseed.com>
2010-09-25 12:02:59 +0000

    Ensure that submit button is replaced with disabled version properly

--- uc_optional_checkout_review.js
+++ uc_optional_checkout_review.js
@@ -10,7 +10,7 @@
  */
 Drupal.behaviors.ucSubmitOrderThrobberNoReview = function(context) {
   $('form#uc-cart-checkout-form input#edit-continue:not(.ucSubmitOrderThrobberNoReview-processed)', context).addClass('ucSubmitOrderThrobberNoReview-processed').click(function() {
-    $(this).clone().insertAfter(this).attr('disabled', true).after('<span class="ubercart-throbber">&nbsp;&nbsp;&nbsp;&nbsp;</span>').end().hide();
+    $(this).clone().attr('disabled', true).insertAfter(this).after('<span class="ubercart-throbber">&nbsp;&nbsp;&nbsp;&nbsp;</span>').end().end().hide();
     $('#uc-cart-checkout-form #edit-cancel').attr('disabled', true);
   });
 }
